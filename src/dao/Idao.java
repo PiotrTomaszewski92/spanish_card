59ca128595dd56bb8e1a29541fcdbcc1461f91fe
PiotrTomaszewski92 <piotrekha15@gmail.com>
2018-06-27 17:13:09 +0200

    prepared packages and files to connection with DB

---
+++ src/dao/Idao.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * Created by Procislaw on 27.06.2018.
+ */
+public interface Idao {
+    void getAllWords();
+}
---
+++ src/dao/MysqlDao.java
@@ -0,0 +1,11 @@
+package dao;
+
+/**
+ * Created by Procislaw on 27.06.2018.
+ */
+public class MysqlDao implements Idao {
+    @Override
+    public void getAllWords() {
+
+    }
+}
