10ab281a2b72d971f5859be539ea67b5b356e4aa
Piotr Tomaszewski <tomaszewski@MacBook-Pro-Piotr.local>
2018-07-02 10:25:59 +0200

    create connenction and get basic data

--- src/main/java/dao/Idao.java
+++ src/main/java/dao/Idao.java
@@ -1,8 +1,10 @@
 package dao;
 
+import java.sql.SQLException;
+
 /**
  * Created by Procislaw on 27.06.2018.
  */
 public interface Idao {
-    void getAllWords(); 
+    void getAllWords() throws SQLException;
 }
--- src/main/java/dao/MysqlDao.java
+++ src/main/java/dao/MysqlDao.java
@@ -1,11 +1,37 @@
 package dao;
 
+import java.sql.*;
+
 /**
  * Created by Procislaw on 27.06.2018.
  */
 public class MysqlDao implements Idao {
-    @Override
-    public void getAllWords() {
+   private Connection conn = new MysqlConn().getConnection();
+
+
+   
+
+
+    public void getAllWords() throws  SQLException{
+
+        PreparedStatement preparedStatement = conn.prepareStatement("SELECT * from words");
+        ResultSet rs = preparedStatement.executeQuery();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+
+        while (rs.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) System.out.print(",  ");
+                String columnValue = rs.getString(i);
+                System.out.print(columnValue + " " + rsmd.getColumnName(i));
+            }
+            System.out.println("");
+        }
+
+
+
+
+
 
     }
 }
